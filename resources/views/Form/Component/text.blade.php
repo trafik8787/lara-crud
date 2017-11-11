bbca277c566d785639f5c3673dfbb60270bf2dd5
Shtirlic <trafik8787@gmail.com>
2017-11-11 19:08:31 +0200

    fix bags HTML text field

--- resources/views/Form/Component/text.blade.php
+++ resources/views/Form/Component/text.blade.php
@@ -2,6 +2,6 @@
 <div class="form-group">
     <label for="{{$obj->name}}" class="col-md-1 control-label">{{$obj->label}}</label>
     <div class="col-md-9">
-        <input type="{{$obj->type}}" name="{{$obj->name}}" title="{{$obj->title}}" value="{!! $obj->value !!}" class="form-control {{$obj->classStyle}}" id="{{$obj->name}}" placeholder="{{$obj->placeholder}}">
+        <input type="{{$obj->type}}" name="{{$obj->name}}" title="{{$obj->title}}" value="{{$obj->value}}" class="form-control {{$obj->classStyle}}" id="{{$obj->name}}" placeholder="{{$obj->placeholder}}">
     </div>
 </div>
